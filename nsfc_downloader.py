21b7e0b4e45384a94f61463cf5da88882c2476e4
xiaoguowen <xiaoguowen@meituan.com>
2020-10-13 15:19:06 +0800

    熟悉代码，修改一些细节

--- nsfc_downloader.py
+++ nsfc_downloader.py
@@ -61,82 +61,59 @@ def clean_filename(string: str) -> str:
     return string.strip()
 
 
-class NsfcDownloader:
+class NsfcDownloader(object):
     i = 1
     download_finish = False
-    download_stats = {}
-
+    download_status = {}
     debug = True
     ratify_info = {}
 
     def __init__(self, out_path, tmp_path):
         self.out_path = out_path
         self.tmp_path = tmp_path
-
-        # 使得相关文件夹存在
-        mkdir_p(out_path)
-        mkdir_p(tmp_path)
+        mkdir_p(self.out_path)
+        mkdir_p(self.tmp_path)
 
     def clear_state(self):
         self.i = 1
         self.download_finish = False
-        self.download_stats = {}
+        self.download_status = {}
 
-    def get_ratify_info_from_nsfc(self, ratify) -> dict:
-        """
-        从官网获取信息
-
-        :param ratify: 申请号
-        :return:
-        """
+    def get_ratify_info_from_nsfc(selfself, ratify) -> dict:
         r = requests.get('http://output.nsfc.gov.cn/baseQuery/data/conclusionProjectInfo/{}'.format(ratify))
-
         try:
             r.raise_for_status()
-            rj = r.json()  # project_info
+            rj = r.json()
             rj['success'] = True
         except Exception as e:
             rj = {'success': False, 'msg': str(e)}
-            # Debug 模式下重新抛出抛出
             if self.debug:
                 raise e
 
         return rj
 
     def get_ratify_info(self, ratify) -> dict:
-        # 检查对象内是不是有缓存
         if ratify not in self.ratify_info:
-            # 检查本地目录是不是有缓存
             project_info_file = os.path.join(self.tmp_path, '{}.json'.format(ratify))
             if os.path.exists(project_info_file):
                 rj = json.load(open(project_info_file, 'r', encoding='utf-8'))
             else:
                 rj = self.get_ratify_info_from_nsfc(ratify)
 
-            # 存入对象缓存
             self.ratify_info[ratify] = rj
-
         return self.ratify_info[ratify]
 
     def download_loop(self, ratify) -> tuple:
-        """
-        下载核心方法
-
-        :param ratify:
-        :return:
-        """
         img_files_list = []
         img_bytes_list = []
-
         ratify_prefix = ratify[:2]
-
         self.i = 1
         while True:
             tmp_file = os.path.join(self.tmp_path, '{}_{}.png'.format(ratify, self.i))
             if os.path.exists(tmp_file):
                 content = open(tmp_file, 'rb').read()
             else:
-                req_url = "http://output.nsfc.gov.cn/report/{}/{}_{}.png".format(ratify_prefix, ratify, self.i)
+                req_url = 'http://output.nsfc.gov.cn/report/{}/{}_{}.png'.format(ratify_prefix, ratify, self.i)
                 print('正在请求页面 {}'.format(req_url))
                 r = requests.get(req_url, timeout=10)
                 if r.status_code == 404:
@@ -150,25 +127,21 @@ class NsfcDownloader:
             img_files_list.append(tmp_file)
             img_bytes_list.append(content)
             self.i += 1
-
         self.download_finish = True
-
         return img_files_list, img_bytes_list
 
     def download(self, ratify):
         status = {'success': False, 'msg': ''}
         print('开始获取项目信息，项目编号： {}'.format(ratify))
-
         ratify_info = self.get_ratify_info(ratify)
         if ratify_info.get('code') != 200 or 'data' not in ratify_info:
             status['msg'] = '项目可能不存在，请重新检查网页 http://output.nsfc.gov.cn/conclusionProject/{} 显示'.format(ratify)
         else:
             project_name = ratify_info['data'].get('projectName', '')
             status['path'] = os.path.join(self.out_path, clean_filename('{} {}.pdf'.format(ratify, project_name)))
-
             if os.path.exists(status['path']):
                 status['success'] = True
-                status['msg'] = 'PDF已存在，请打开 `{}`。'.format(status['path'])
+                status['msb'] = 'PDF已存在， 请打开 `{}`.'.format(status['path'])
             else:
                 try:
                     print('开始下载 {} {}'.format(ratify, project_name))
@@ -181,6 +154,7 @@ class NsfcDownloader:
                         with open(status['path'], "wb") as file_:
                             file_.write(pdf)
                             status['success'] = True
+                            status['msg'] = "下载成功！"
 
                         if self.debug:
                             print('移除临时文件')
